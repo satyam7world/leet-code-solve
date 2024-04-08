f89a1a342aa4b2463fe5f85b4a362696b733ba81
Satyam <58844449+satyam7world@users.noreply.github.com>
2024-04-08 13:35:19 +0530

    somewhat working

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -7,15 +7,20 @@ public class Solution {
         // check if last digit is not nine
         var lastDigit = digits[digits.length - 1];
         var invokeCarry = false;
-        var arrayInt = new int[digits.length];
+        var arrayInt = new int[digits.length + 1];
         for (int i = digits.length; i > 0; i--) {
             var currentDigit = digits[i - 1];
             var currentUpdate = currentDigit;
+            if (i == 1 && invokeCarry) {
+                System.out.println("need extra digit");
+                arrayInt[digits.length] = currentDigit + 1;
+            }
             if (i == digits.length || invokeCarry) {
                 var increment = currentDigit + 1;
                 if (increment == 10) { // send a carry
                     System.out.println(0);
                     invokeCarry = true;
+//                    arrayInt[digits.length - i] = increment;
                     continue;
                 }
                 currentUpdate = increment;
