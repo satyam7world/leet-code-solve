65b8c2baad5007ec92d0f149c647d6eb79bef7e0
Satyam <58844449+satyam7world@users.noreply.github.com>
2024-04-08 13:58:16 +0530

    more improvement

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         var sol = new Solution();
-        int[] helloIndia = sol.plusOne(new int[]{9, 9});
+        int[] helloIndia = sol.plusOne(new int[]{1, 2, 9});
         System.out.println("final response");
         System.out.println(Arrays.toString(helloIndia));
     }
--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -6,40 +6,45 @@ public class Solution {
     public int[] plusOne(int[] digits) {
         // check if last digit is not nine
         var lastDigit = digits[digits.length - 1];
-        var invokeCarry = false;
-        var arrayInt = new int[digits.length + 1];
-        for (int i = digits.length; i > 0; i--) {
-            var currentDigit = digits[i - 1];
-            var currentUpdate = currentDigit;
-            if (i == 1 && invokeCarry) {
-                System.out.println("need extra digit");
+        if (lastDigit != 9) {
+            digits[digits.length - 1] = lastDigit + 1;
+            return digits;
+        } else {
+            var invokeCarry = false;
+            var arrayInt = new int[digits.length + 1];
+            for (int i = digits.length; i > 0; i--) {
+                var currentDigit = digits[i - 1];
+                var currentUpdate = currentDigit;
+                if (i == 1 && invokeCarry) {
+                    System.out.println("need extra digit");
 //                arrayInt[digits.length] = currentDigit + 1;
-                arrayInt[digits.length] = 1;
-            }
-            if (i == digits.length || invokeCarry) {
-                var increment = currentDigit + 1;
-                if (increment == 10) { // send a carry
-                    System.out.println(0);
-                    invokeCarry = true;
-//                    arrayInt[digits.length - i] = increment;
-                    continue;
+                    arrayInt[digits.length] = 1;
                 }
-                currentUpdate = increment;
+                if (i == digits.length || invokeCarry) {
+                    var increment = currentDigit + 1;
+                    if (increment == 10) { // send a carry
+                        System.out.println(0);
+                        invokeCarry = true;
+//                    arrayInt[digits.length - i] = increment;
+                        continue;
+                    }
+                    currentUpdate = increment;
 //                arrayInt[digits.length - i] = increment;
+                }
+                arrayInt[digits.length - i] = currentUpdate;
+                invokeCarry = false;
             }
-            arrayInt[digits.length - i] = currentUpdate;
-            invokeCarry = false;
+
+            System.out.println("new thing");
+            System.out.println(Arrays.toString(arrayInt));
+
+            var solution = new int[arrayInt.length];
+
+            for (int i = 0; i < arrayInt.length; i++) {
+                solution[i] = arrayInt[arrayInt.length - 1 - i];
+            }
+
+            return solution;
         }
-
-        System.out.println("new thing");
-        System.out.println(Arrays.toString(arrayInt));
-
-        var solution = new int[arrayInt.length];
-
-        for (int i = 0; i < arrayInt.length; i++) {
-            solution[i] = arrayInt[arrayInt.length - 1 - i];
-        }
-
-        return solution;
     }
 }
