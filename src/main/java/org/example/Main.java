ba4ec27becc372cd79427c311a214f55c376b556
Satyam <58844449+satyam7world@users.noreply.github.com>
2024-04-10 07:39:25 +0530

    question  26

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         var sol = new Solution();
-        int[] helloIndia = sol.plusOne(new int[]{1, 2, 9});
+        int helloIndia = sol.removeDuplicates(new int[]{1, 2, 9});
         System.out.println("final response");
-        System.out.println(Arrays.toString(helloIndia));
+        System.out.println(helloIndia);
     }
 }
--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,50 +1,16 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
 
 public class Solution {
-    public int[] plusOne(int[] digits) {
-        // check if last digit is not nine
-        var lastDigit = digits[digits.length - 1];
-        if (lastDigit != 9) {
-            digits[digits.length - 1] = lastDigit + 1;
-            return digits;
-        } else {
-            var invokeCarry = false;
-            var arrayInt = new int[digits.length + 1];
-            for (int i = digits.length; i > 0; i--) {
-                var currentDigit = digits[i - 1];
-                var currentUpdate = currentDigit;
-                if (i == 1 && invokeCarry) {
-                    System.out.println("need extra digit");
-//                arrayInt[digits.length] = currentDigit + 1;
-                    arrayInt[digits.length] = 1;
-                }
-                if (i == digits.length || invokeCarry) {
-                    var increment = currentDigit + 1;
-                    if (increment == 10) { // send a carry
-                        System.out.println(0);
-                        invokeCarry = true;
-//                    arrayInt[digits.length - i] = increment;
-                        continue;
-                    }
-                    currentUpdate = increment;
-//                arrayInt[digits.length - i] = increment;
-                }
-                arrayInt[digits.length - i] = currentUpdate;
-                invokeCarry = false;
-            }
-
-            System.out.println("new thing");
-            System.out.println(Arrays.toString(arrayInt));
-
-            var solution = new int[arrayInt.length];
-
-            for (int i = 0; i < arrayInt.length; i++) {
-                solution[i] = arrayInt[arrayInt.length - 1 - i];
-            }
-
-            return solution;
+    public int removeDuplicates(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int i : nums) {
+            set.add(i);
         }
+        return set.size();
     }
 }
