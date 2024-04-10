91738d54e6e271a42f8bb80fe9719ec64e8966f7
Satyam <58844449+satyam7world@users.noreply.github.com>
2024-04-10 21:17:44 +0530

    more works

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -8,19 +8,25 @@ import java.util.regex.Pattern;
 public class Solution {
     public int myAtoi(String s) {
         var pattern = Pattern.compile("[0-9]");
-        Matcher matcher = pattern.matcher(s);
+        Matcher matcher = pattern.matcher(s.trim());
         List<Integer> integers = new ArrayList<>();
 
         while (matcher.find()) {
 //            System.out.println(matcher.group());
             integers.add(Integer.valueOf(matcher.group()));
         }
+        List<Integer> correctFormInt = new ArrayList<>();
         int multiplier = 1;
         for (int i = integers.size(); i > 0; i--) {
 //            var oppo = integers.size() - i;
-
+            correctFormInt.add(integers.get(i - 1) * multiplier);
             multiplier *= 10;
         }
-        return 0;
+
+        Integer finalAnswer = 0;
+        for (Integer i : correctFormInt) {
+            finalAnswer += i;
+        }
+        return finalAnswer;
     }
 }
