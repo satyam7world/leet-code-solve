281043184d4f797e1546462501fec6f735f62ca3
Satyam <58844449+satyam7world@users.noreply.github.com>
2024-04-10 21:12:24 +0530

    question  26

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         var sol = new Solution();
-        int helloIndia = sol.removeDuplicates(new int[]{1, 2, 9});
+        int helloIndia = sol.myAtoi("4falfjla3234");
         System.out.println("final response");
         System.out.println(helloIndia);
     }
--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,16 +1,26 @@
 package org.example;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Solution {
-    public int removeDuplicates(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
-        for(int i : nums) {
-            set.add(i);
+    public int myAtoi(String s) {
+        var pattern = Pattern.compile("[0-9]");
+        Matcher matcher = pattern.matcher(s);
+        List<Integer> integers = new ArrayList<>();
+
+        while (matcher.find()) {
+//            System.out.println(matcher.group());
+            integers.add(Integer.valueOf(matcher.group()));
         }
-        return set.size();
+        int multiplier = 1;
+        for (int i = integers.size(); i > 0; i--) {
+//            var oppo = integers.size() - i;
+
+            multiplier *= 10;
+        }
+        return 0;
     }
 }
