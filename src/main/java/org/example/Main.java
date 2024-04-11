743cb9c1ad0a21a147446cf2067051fccd847d74
Satyam <58844449+satyam7world@users.noreply.github.com>
2024-04-11 07:27:55 +0530

    question 10

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         var sol = new Solution();
-        int helloIndia = sol.myAtoi("4falfjla3234");
+        var helloIndia = sol.isMatch("aa", "a");
         System.out.println("final response");
         System.out.println(helloIndia);
     }
--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -6,27 +6,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Solution {
-    public int myAtoi(String s) {
-        var pattern = Pattern.compile("[0-9]");
-        Matcher matcher = pattern.matcher(s.trim());
-        List<Integer> integers = new ArrayList<>();
+    public boolean isMatch(String s, String p) {
 
-        while (matcher.find()) {
-//            System.out.println(matcher.group());
-            integers.add(Integer.valueOf(matcher.group()));
-        }
-        List<Integer> correctFormInt = new ArrayList<>();
-        int multiplier = 1;
-        for (int i = integers.size(); i > 0; i--) {
-//            var oppo = integers.size() - i;
-            correctFormInt.add(integers.get(i - 1) * multiplier);
-            multiplier *= 10;
-        }
-
-        Integer finalAnswer = 0;
-        for (Integer i : correctFormInt) {
-            finalAnswer += i;
-        }
-        return finalAnswer;
+        return false;
     }
 }
