54cb6b896e8ba3b277c024edf80a507abf905b06
Satyam <58844449+satyam7world@users.noreply.github.com>
2024-04-13 10:14:16 +0530

    checkpoint

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,8 +5,9 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         var sol = new Solution();
-        var helloIndia = sol.reverseWords("hello"
-        );
+        var helloIndia = sol.trap(new int[]{
+                0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1
+        });
         System.out.println("final response");
         System.out.println(helloIndia);
     }
--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -3,21 +3,28 @@ package org.example;
 import java.util.*;
 
 class Solution {
-    public String reverseWords(String s) {
-        String result = "";
-        int indexO = 0;
-        for (String fraction : s.split(" ")) {
-            for (int i = fraction.length() - 1; i >= 0; i--) {
-                result = result.concat(String.valueOf(fraction.charAt(i)));
+    public int trap(int[] height) {
+        // find MaxLeft
+        int[] maxLeft = new int[height.length];
+        int lastMax = 0;
+
+        for (int i = 0; i < height.length; i++) {
+            if (height[i] > lastMax) {
+                maxLeft[i] = lastMax;
+                lastMax = height[i];
             }
-
-            indexO++;
-
-            if (indexO < s.split(" ").length) {
-                result = result.concat(" ");
-            }
-
         }
-        return result;
+        int[] minValue = new int[height.length];
+        // find minimum of both
+        int currentRightMax = 0;
+        for (int i = height.length - 1; i > 0; i--) {
+            if (height[i] > currentRightMax) {
+                minValue[height.length - i] = Math.min(height[i], maxLeft[height.length - i]);
+            }
+        }
+
+        System.out.println(Arrays.toString(maxLeft));
+
+        return -1;
     }
 }
